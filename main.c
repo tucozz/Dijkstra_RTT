dd0389e82718097e76a3be1766ebec4c53e3371e
Tucozz <tutuestefanato@gmail.com>
2023-10-19 01:01:48 -0300

    double main fix

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
-//TODO: MAIN FUNCTION
+#include "headers/input_reader.h"
+#include "headers/output_writer.h"
+#include "headers/graph.h"
+#include "headers/heap.h"
 
-//TODO: LE OS ARQUIVOS USANDO INPUT READER E SALVA NA STRUCT RTT
+int main(int argc, char **argv)
+{
+    if(argc != 3){
+        printf("Usage: ./trab1 <arquivo_input> <aquivo_output>\n");
+        return 1;
+    }
 
-//TODO: CALCULA TUDO USANDO RTT
+    //Le o input
+    Graph *graph = graph_read(argv[1]);
 
-//TODO: CALCULA TUDO USANDO RTT APROXIMADO
+    //Teste heap
+    Heap *heap = heap_construct(5);
 
-//TODO: IMPRIME COM OUTPUT WRITER
+    for(int i = 0; i < 5; i++){
+        heap_push(heap, i, i*-1);
+        heap_debug(heap);
+    }
+
+    //Escreve o output
+    write_output_in_file_(argv[2]);
+
+    return 0;
+}
