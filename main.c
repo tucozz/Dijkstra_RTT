3035a358e62768a5964dd6d2c5e9c6a2b32f96bf
Tucozz <tutuestefanato@gmail.com>
2023-10-27 18:46:35 -0300

    delete heap test

--- main.c
+++ main.c
@@ -13,13 +13,6 @@ int main(int argc, char **argv)
     //Le o input
     Graph *graph = graph_read(argv[1]);
 
-    //Teste heap
-    Heap *heap = heap_construct(5);
-
-    //TODO: o heap parece estar funcionando
-    //fazer um iterador para o grafo, pra poder adicionar aresta por aresta no heap
-    //adicionar todas arestas como prioridade DOUBLE_MAX ou coisa assim, e só s como 0
-    //ver se funciona mesmo
 
     //Escreve o output
     write_output_in_file_(argv[2]);
