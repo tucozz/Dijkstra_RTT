174398de3d0990b73c2bfde6f88878e1471f5e9e
Aurea <aureasoliveiraa@gmail.com>
2023-10-29 18:52:15 -0300

    main update

--- main.c
+++ main.c
@@ -19,27 +19,30 @@ int main(int argc, char **argv)
     Rtt *rtt = rtt_run(graph);
     Rtt *rtt2 = rtt_star_run(graph);
 
-    //TODO: Imprimir o arquivo de saida de modo correto
-    //Modo correto: Três colunas. Uma para a origem, uma para o destino e uma para a razao entre custos RTT e RTT*
-    //A estrutura RTT já tem os valores de RTT e RTT* para cada par de vértices. Basta calcular a razão e imprimir
+    // TODO: Imprimir o arquivo de saida de modo correto
+    // Modo correto: Três colunas. Uma para a origem, uma para o destino e uma para a razao entre custos RTT e RTT*
+    // A estrutura RTT já tem os valores de RTT e RTT* para cada par de vértices. Basta calcular a razão e imprimir
 
     double **S_to_C_rtt = rtt_get_S_to_C_rtt(rtt);
     double **S_to_C_rtt_star = rtt_get_S_to_C_rtt(rtt2);
 
-    //printf("%lf" , S_to_C_rtt[0][0] / S_to_C_rtt_star[0][0]);
+    // printf("%lf" , S_to_C_rtt[0][0] / S_to_C_rtt_star[0][0]);
 
-    //PRINT PARA DEBUG:
-    //TODO: Alguns valores estão dando infinito. Verificar o que está acontecendo
-    //provavelmente é o dijkstra em algum caso específico, mas verificar isso aí.
-    for(int i = 0; i < graph_get_num_servers(graph); i++){
-        for(int j = 0; j < graph_get_num_clients(graph); j++){
-            double rtt_ratio = S_to_C_rtt[i][j] / S_to_C_rtt_star[i][j];
+    // PRINT PARA DEBUG:
+    // TODO: Alguns valores estão dando infinito. Verificar o que está acontecendo
+    // provavelmente é o dijkstra em algum caso específico, mas verificar isso aí.
+    for (int i = 0; i < graph_get_num_servers(graph); i++)
+    {
+        for (int j = 0; j < graph_get_num_clients(graph); j++)
+        {
+            //printf("%lf %lf\n", S_to_C_rtt[i][j], S_to_C_rtt_star[i][j]);
+            double rtt_ratio =  S_to_C_rtt_star[i][j]/S_to_C_rtt[i][j];
             printf("%d %d %lf\n", graph_get_server_index(graph, i), graph_get_client_index(graph, j), rtt_ratio);
         }
     }
 
     // Escreve o output
-    //write_output_in_file_(argv[2]);
+    // write_output_in_file_(argv[2]);
 
     // Libera a memoria
     graph_destroy(graph);
