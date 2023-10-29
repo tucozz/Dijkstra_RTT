20a8a1a3694302516e8337bff87f3ab2e1176201
Tucozz <tutuestefanato@gmail.com>
2023-10-28 21:34:41 -0300

    generate getters and setters

--- src/data_structures/graph.c
+++ src/data_structures/graph.c
@@ -27,6 +27,15 @@ Graph *graph_construct(size_t num_vertex, size_t n_server, size_t n_client, size
     return graph;
 }
 
+void graph_destroy(Graph *graph)
+{
+    free(graph->vertex_type);
+    free(graph->servers_idx);
+    free(graph->clients_idx);
+    free(graph->monitors_idx);
+    free(graph);
+}
+
 void add_edge_u_to_v(struct Graph *graph, int u, int v, double weight)
 {
     add_neighbor_to_list(graph->adj_list, u, v, weight);
@@ -52,7 +61,72 @@ int graph_get_num_vertex(Graph *graph)
     return graph->n_vertex;
 }
 
+int graph_get_num_servers(Graph *graph)
+{
+    return sizeof(graph->servers_idx) / sizeof(int);
+}
+
+int graph_get_num_clients(Graph *graph)
+{
+    return sizeof(graph->clients_idx) / sizeof(int);
+}
+
+int graph_get_num_monitors(Graph *graph)
+{
+    return sizeof(graph->monitors_idx) / sizeof(int);
+}
+
+int *graph_get_servers(Graph *graph)
+{
+    return graph->servers_idx;
+}
+
+int *graph_get_clients(Graph *graph)
+{
+    return graph->clients_idx;
+}
+
+int *graph_get_monitors(Graph *graph)
+{
+    return graph->monitors_idx;
+}
+
+char *graph_get_vertex_type(Graph *graph)
+{
+    return graph->vertex_type;
+}
+
 adjList *graph_get_arr_adjList(Graph *g)
 {
     return g->adj_list;
+}
+
+int graph_get_server_index(Graph *graph, int server)
+{
+    return graph->servers_idx[server];
+}
+
+int graph_get_client_index(Graph *graph, int client)
+{
+    return graph->clients_idx[client];
+}
+
+int graph_get_monitor_index(Graph *graph, int monitor)
+{
+    return graph->monitors_idx[monitor];
+}
+
+void graph_set_server_idx(Graph *graph, int server, int idx)
+{
+    graph->servers_idx[server] = idx;
+}
+
+void graph_set_client_idx(Graph *graph, int client, int idx)
+{
+    graph->clients_idx[client] = idx;
+}
+
+void graph_set_monitor_idx(Graph *graph, int monitor, int idx)
+{
+    graph->monitors_idx[monitor] = idx;
 }
